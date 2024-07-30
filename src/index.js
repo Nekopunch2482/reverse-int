41282e238d89b3ab78e0120f7b790d42324fbd57
Nekopunch <Nekopunch2482@yandex.ru>
2024-07-30 23:20:52 +0300

    feat: implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
-}
+/////// VARIANT 2
+module.exports = function reverse(n) {
+    let buf = Math.abs(n);
+    let accum = 0;
+
+    while (buf != 0) {
+        accum = Math.round((accum + ((buf / 10) % 1)) * 10);
+        buf = Math.trunc(buf / 10);
+    }
+
+    return accum;
+};
+
+/////// VARIANT 1
+// module.exports = function reverse(n) {
+//     return Number(Math.abs(n).toString().split("").reverse().join(""));
+// };
